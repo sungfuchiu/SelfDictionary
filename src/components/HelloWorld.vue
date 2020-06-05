79c61c02376923f1b2b61e59066e89597d041c48
joseph351619 <sungfuchiu@gmail.com>
2020-06-05 23:58:33 +0800

    Remove the other obsolete array

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -121,20 +121,6 @@
       searchResult: [],
       isSearchFail:false,
       newExplanation: '',
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
       ],
     }),
   }
